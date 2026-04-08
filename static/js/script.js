b27e1b2932e63c988c212dba71dd92adc55b234a
neha523 <nehapathare523@gmail.com>
2026-04-08 18:19:52 +0800

    feat: build meetup-style homepage with event cards and map

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,34 @@
-var map = L.map('map').setView([-31.95, 115.86], 13);
+var map = L.map('map').setView([-31.9505, 115.8605], 11);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Example marker
-L.marker([-31.95, 115.86]).addTo(map)
-    .bindPopup("Sample Event")
-    .openPopup();
+var events = [
+    {
+        title: 'Tech Networking Night',
+        coords: [-31.9523, 115.8613],
+        location: 'Perth CBD'
+    },
+    {
+        title: 'Beginner Yoga in the Park',
+        coords: [-31.9617, 115.8327],
+        location: 'Kings Park'
+    },
+    {
+        title: 'Startup Pitch Evening',
+        coords: [-31.9478, 115.8233],
+        location: 'Subiaco'
+    },
+    {
+        title: 'Sunset Beach Meetup',
+        coords: [-31.9937, 115.7528],
+        location: 'Cottesloe Beach'
+    }
+];
+
+events.forEach(function(event) {
+    L.marker(event.coords)
+        .addTo(map)
+        .bindPopup('<strong>' + event.title + '</strong><br>' + event.location);
+});
